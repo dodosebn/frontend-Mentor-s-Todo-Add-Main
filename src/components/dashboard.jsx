7140a0b079429482ef9d9f9ee19752ac094b9fb2
orji Dominion <orjidominion32@gmail.com>
2024-09-19 13:24:26 +0100

    likkle update

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,9 +1,6 @@
-// Dashboard.jsx
 import React from 'react';
 import deleteIcon from '../images/icon-cross.svg';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-
-const Dashboard = ({ items, updateItem, deleteItem, reorderItems }) => {
+const Dashboard = ({ items, updateItem, deleteItem }) => {
   if (!items || !Array.isArray(items)) {
     return <p>Get productive! Add a todo</p>;
   }
@@ -23,76 +20,43 @@ const Dashboard = ({ items, updateItem, deleteItem, reorderItems }) => {
   };
 
   const completedCount = items.filter(item => item.isChecked).length;
-  // const handleDelCompletedItems = () => {
-  //   const newItems = items.filter(item => !item.isChecked);
-  //   setItems(newItems);
-  // };
-
-  const onDragEnd = (result) => {
-    const { source, destination } = result;
-
-    if (!destination) return;
-
-    if (source.index === destination.index) return;
-
-    const newItems = Array.from(items);
-    const [movedItem] = newItems.splice(source.index, 1);
-    newItems.splice(destination.index, 0, movedItem);
-
-    reorderItems(newItems);
-  };
 
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="droppable-todo-list">
-        {(provided) => (
-          <div {...provided.droppableProps} ref={provided.innerRef}>
-            {items.map((item, index) => (
-              <Draggable key={item.id} draggableId={item.id} index={index}>
-                {(provided) => (
-                  <div
-                    className="input-container dashboard-item"
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                  >
-                    <div className="input-wrapper">
-                      <div className="custom-checkbox" onClick={() => handleCheckboxChange(index)}>
-                        <input
-                          type="checkbox"
-                          className="checkbox-hidden"
-                          checked={item.isChecked}
-                          readOnly
-                        />
-                        <label className="checkbox-label"></label>
-                      </div>
-                      <input 
-                        type="text" 
-                        value={item.text}
-                        className={`text-input ${item.isChecked ? 'completed' : ''}`}
-                        onChange={(event) => handleTextChange(index, event)} 
-                      />
-                      <button onClick={() => handleDelete(index)} className="delete-button">
-                        <img src={deleteIcon} alt='Delete' />
-                      </button>
-                    </div>
-                  </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
+    <div>
+      {items.length === 0 ? (
+        <p>Get productive! Add a todo</p>
+      ) : (
+        items.map((item, index) => (
+          <div key={index} className="input-container dashboard-item">
+            <div className="input-wrapper">
+              <div className="custom-checkbox" onClick={() => handleCheckboxChange(index)}>
+                <input
+                  type="checkbox"
+                  className="checkbox-hidden"
+                  checked={item.isChecked}
+                  readOnly
+                />
+                <label className="checkbox-label"></label>
+              </div>
+              <input 
+                type="text" 
+                value={item.text}
+                className={`text-input ${item.isChecked ? 'completed' : ''}`}
+                onChange={(event) => handleTextChange(index, event)} 
+              />
+              <button onClick={() => handleDelete(index)} className="delete-button"><img src={deleteIcon} alt='colo'/></button>
+            </div>
           </div>
-        )}
-      </Droppable>
-      <article className="total-count">
+        ))
+      )}
+      <article className=" total-count">
         <div className="downbelow">
           <div className="total-text">All: {items.length}</div>
           <div className='active'>Active</div>
-          <div className="total-text">Completed: {completedCount}</div>
-          {/* <button onClick={handleDelCompletedItems}>Delete Completed Items</button> */}
+          <div className="total-text"> Completed: {completedCount}</div>
         </div>
       </article>
-    </DragDropContext>
+    </div>
   );
 };
 
